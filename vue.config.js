ba63ff38023b4e7c262014d3de97d8af24aa32df
Kiddle <a1044735733@163.com>
2024-08-24 09:12:38 +0800

    feat: vue.config.js配置

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,32 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+
+  publicPath: "./",
+ 
+  outputDir: 'disk',
+ 
+  assetsDir: 'assets',
+ 
+ 
+  transpileDependencies: true,
+ 
+  devServer: {
+    hot: true, //热加载
+    host: 'localhost',
+    port: 8080, //端口
+    https: false, //false关闭https，true为开启
+    open: true, //自动打开浏览器
+    proxy: {
+      '/api': {
+        target: '',
+        ws: true,
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': '/'
+        }
+      },
+    }
+  },
+ 
+  
 })
