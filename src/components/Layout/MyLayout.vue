14e1d7ca6f70de79eb2d9285cb51d7af7a570947
Kiddle <a1044735733@163.com>
2024-08-25 09:57:32 +0800

    feat: 静态页面

--- src/components/Layout/MyLayout.vue
+++ src/components/Layout/MyLayout.vue
@@ -17,7 +17,147 @@
 
       </el-header>
       <el-main>
-        <el-table></el-table>
+        <div>
+          <el-form
+            :inline="true"
+            :model="formInline"
+            class="demo-form-inline"
+          >
+            <el-form-item label="姓名">
+              <el-input
+                v-model="formInline.username"
+                placeholder="姓名"
+                clearable
+                maxlength="5"
+                style="width:200px"
+              />
+            </el-form-item>
+            <el-form-item label="性别">
+              <el-select
+                v-model="formInline.sex"
+                placeholder="请选择性别"
+                clearable
+                style="width:200px"
+              >
+                <el-option
+                  label="男"
+                  value="1"
+                />
+                <el-option
+                  label="女"
+                  value="0"
+                />
+              </el-select>
+            </el-form-item>
+            <el-form-item label="状态">
+              <el-select
+                v-model="formInline.status"
+                placeholder="请选择状态"
+                clearable
+                style="width:200px"
+              >
+                <el-option
+                  label="启用"
+                  value="1"
+                />
+                <el-option
+                  label="禁用"
+                  value="0"
+                />
+              </el-select>
+            </el-form-item>
+            <el-form-item>
+              <el-button
+                type="primary"
+                @click="onSubmit"
+              >查询</el-button>
+              <el-button
+                type="primary"
+                @click="handleReset"
+              >重置</el-button>
+              <el-button
+                type="primary"
+                color="green"
+                @click="handleReset"
+              >新增</el-button>
+              <el-button
+                type="primary"
+                color="red"
+                @click="handleReset"
+              >批量删除</el-button>
+            </el-form-item>
+          </el-form>
+        </div>
+        <el-table
+          ref="multipleTableRef"
+          :data="tableData"
+          @selection-change="handleSelectionChange"
+          style="width:90%;margin:auto"
+        >
+          <el-table-column type="selection" />
+
+          <el-table-column
+            label="id"
+            property="id"
+          />
+          <el-table-column
+            property="name"
+            label="姓名"
+          />
+          <el-table-column
+            property="sex"
+            label="性别"
+          />
+          <el-table-column
+            property="年龄"
+            label="age"
+          />
+          <el-table-column
+            property="手机号"
+            label="phone"
+          />
+          <el-table-column
+            property="创建日期"
+            label="createTime"
+          />
+          <el-table-column
+            property="修改日期"
+            label="updateTime"
+          />
+          <el-table-column
+            fixed="right"
+            label="操作"
+          >
+            <template #default>
+              <el-button
+                link
+                type="primary"
+                size="small"
+                @click="handleClick"
+              >
+                编辑
+              </el-button>
+              <el-button
+                link
+                type="primary"
+                size="small"
+              >删除</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+        <div class="pagination">
+          <el-pagination
+            v-model:current-page="pageNum"
+            v-model:page-size="pageSize"
+            :page-sizes="[10, 50, 100, 150]"
+            :size="size"
+            layout="total, sizes, prev, pager, next, jumper"
+            :total="150"
+            @size-change="handleSizeChange"
+            @current-change="handleCurrentChange"
+          />
+        </div>
+
       </el-main>
     </el-container>
   </el-container>
@@ -25,22 +165,59 @@
 </template>
 
 <script lang="js" setup>
-import { onMounted } from 'vue';
-// import { test } from '@/api/user';
+import { onMounted,reactive,ref } from 'vue';
+const formInline = reactive({
+  username: '',
+  sex: '0',
+  status: '1',
+})
+const multipleSelection = ref([])
+const pageNum = ref(1);
+const pageSize = ref(10);
+const handleSelectionChange = (val) => {
+  multipleSelection.value = val
+}
+const tableData = [
+  {
+		id:1,
+    date: '2016-05-03',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-02',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-04',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-01',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-08',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-06',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-07',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+]
 
-// const getData = async (data)=>{
-// 	const res = await test(data);
-// 	console.log('123123',res);
-
-// }
 onMounted(()=>{
-	// const data = {
-	// 	page:1,
-	// 	pageSize:10,
-	// 	name:'',
-	// }
 
-	// 123getData(data);
 
 });
 
@@ -61,5 +238,11 @@ onMounted(()=>{
   background-color: #30354f;
   border-right: 1px solid #30354f;
   color: white !important;
+}
+.pagination {
+  display: flex;
+  flex-direction: row-reverse;
+  margin-top: 20px;
+  margin-right: 55px;
 }
 </style>
